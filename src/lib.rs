4253120b068f22780bd542f34a4a6ed3b9e83f0a
callqh <528851597@qq.com>
2024-06-27 16:00:04 +0800

    feat: refactor

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ use farmfe_core::{
   serde_json::{self, Value},
 };
 use farmfe_macro_plugin::farm_plugin;
+use select::{
+  document::Document,
+  predicate::{Attr, Class, Name, Predicate, Text},
+};
 use serde;
 use std::collections::HashMap;
 
@@ -24,12 +28,12 @@ pub struct Options {
 }
 
 impl FarmPluginHtmlTemplate {
-  fn new(config: &Config, options: String) -> Self {
+  pub fn new(_config: &Config, options: String) -> Self {
     let my_options: Options = serde_json::from_str(&options)
       .expect(&format!("failed to parse template options: {:?}", options));
     Self {
       options: my_options,
-      re: Regex::new(r"\$\{(\w+)\}\$").unwrap(),
+      re: Regex::new(r#"(?:\$\{(\w+)\}\$|['"]\{(\w+)\}\$['"])"#).unwrap(),
     }
   }
 }
@@ -46,20 +50,46 @@ impl Plugin for FarmPluginHtmlTemplate {
   ) -> farmfe_core::error::Result<Option<farmfe_core::plugin::PluginTransformHookResult>> {
     // only handle html file
     if ModuleType::Html == param.module_type && self.options.template == param.resolved_path {
-      let content = param.content.clone();
-      let mut result = String::new();
-      for capture in self.re.captures_iter(&content) {
-        let captured_var = capture.get(1).unwrap().as_str();
-        let custom_data = &self.options.data;
+      // let content = param.content.clone();
+      let document = Document::from(param.content.as_str());
+      println!("({:?})", param.resolved_path);
+
+      for node in document.find(Text) {
+        println!(
+          "({:?})===>{:?}",
+          self.re.is_match(&node.text()),
+          node.text()
+        );
+        // for capture in self.re.captures_iter(&content) {
+        //   let captured_var = capture.get(1).unwrap().as_str();
+        //   let custom_data = &self.options.data;
 
         // If the user configures the data, it will be replaced.
-        if custom_data.contains_key(captured_var) {
-          if let Some(replacement) = custom_data.get(captured_var) {
-            let trim_quote_str = replacement.as_str().unwrap().trim_matches('"');
-            result = self.re.replace(&content, trim_quote_str).to_string();
+        // if custom_data.contains_key(captured_var) {
+        //   if let Some(replacement) = custom_data.get(captured_var) {
+        //     println!("key:{:?},val:{:?}", captured_var, replacement);
+        //     match replacement.clone() {
+        //       Value::Array(arr) => {
+        //         result = self.re.replace(&content, &arr).to_string();
+        //       }
+        //       _ => {}
+        //     }
+        //     let trim_quote_str = replacement.as_str().unwrap().trim_matches('"');
+        //     result = self.re.replace(&content, trim_quote_str).to_string();
+        //   }
+        // }
+        let content = node.text().clone();
+        let old = self.re.captures(&content);
+        match old {
+          Some(txt) => {
+            node.text().replace(txt.get(1).unwrap().as_str(), "1111");
           }
+          _ => {}
         }
       }
+      let result = String::new();
+
+      // }
 
       return Ok(Some(PluginTransformHookResult {
         content: result,
